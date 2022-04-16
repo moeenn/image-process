b0c3952dce0485c9078e3b2427c53c1677720aa1
moeenn <moeen.v8@gmail.com>
2022-04-16 17:13:24 +0500

    added tag to timer class

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,12 +1,14 @@
 #include "./Timer.hpp"
 
-Timer::Timer() { m_start = std::chrono::high_resolution_clock::now(); }
+Timer::Timer(const std::string &tag) : m_tag(tag) {
+  m_start = std::chrono::high_resolution_clock::now();
+}
 
 Timer::~Timer() {
   m_end = std::chrono::high_resolution_clock::now();
   auto duration =
       std::chrono::duration_cast<std::chrono::milliseconds>(m_end - m_start);
 
-  std::cout << colors::FBLK << "[elapsed] " << duration.count() << " ms"
+  std::cout << colors::FBLK << '[' << m_tag << "] " << duration.count() << " ms"
             << colors::RS << '\n';
 }
--- src/Timer.hpp
+++ src/Timer.hpp
@@ -5,9 +5,10 @@
 
 class Timer {
 private:
+  std::string m_tag;
   std::chrono::time_point<std::chrono::high_resolution_clock> m_start, m_end;
 
 public:
-  Timer();
+  Timer(const std::string& tag = "elapsed");
   ~Timer();
 };
